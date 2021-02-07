23c5dace2b8054baaaba252a4b8e6936b943502e
NeftaliRJ <49695411+NeftaliRJ@users.noreply.github.com>
2021-02-07 02:27:04 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,24 @@
 <template>
   <v-app>
     <LoadingScreen :isLoading="isLoading" />
-    <v-app-bar
-      app
-      color="#0484eb"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          href="https://github.com/vuetifyjs/vuetify/releases/latest"
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://static1.gamerantimages.com/wordpress/wp-content/uploads/2020/06/studio-ghibli-logo.jpg"
-          transition="scale-transition"
-          width="120"
-        />
-      </div>
-      <v-spacer></v-spacer>
-    </v-app-bar>
-    <v-main>
+    <v-main v-if="!isLoading">
+      <v-app-bar
+        app
+        color="#0484eb"
+        dark
+      >
+        <div class="d-flex align-center">
+          <v-img
+            href="https://github.com/vuetifyjs/vuetify/releases/latest"
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            src="https://static1.gamerantimages.com/wordpress/wp-content/uploads/2020/06/studio-ghibli-logo.jpg"
+            transition="scale-transition"
+            width="120"
+          />
+        </div>
+      </v-app-bar>
       <v-container>
         <router-view/>
       </v-container>
@@ -38,8 +37,14 @@ export default {
     LoadingScreen
   },
 
-  data: () => ({
-    //
-  }),
+  data() {
+    return { isLoading: true };
+  },
+
+   mounted() {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 2000);
+  }
 };
 </script>
